5f6ae2a2fc914c1e26fac575620e8ca062713950
caddySpoon <cwb5116@gmail.com>
2022-04-12 23:57:33 +0900

    라우터 변경 중

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import routes from './routes';
+import './App.css';
+import Routes from './Routes';
+import Footer from './pages/Footer'
 
 const App = () => {
   ReactDOM.render(
     <BrowserRouter>
-      <routes />
+      <Routes />
+      <Footer />
     </BrowserRouter>,
     document.getElementById('root'),
   )
